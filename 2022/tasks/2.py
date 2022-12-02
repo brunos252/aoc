d184d32802f8b35a9729f55443693ffbde08a73b
Bruno Sacaric <bruno.sacaric@gmail.com>
2022-12-02 07:08:11 +0100

    day2 2022

---
+++ 2022/tasks/2.py
@@ -0,0 +1,11 @@
+def parse_line(line):
+    line = line.rstrip().split(" ")
+    return [ord(line[0])-65, ord(line[1])-88]
+
+with open("../input/2.txt") as f:
+    lines = [parse_line(line) for line in f.readlines()]
+
+score1 = sum(line[1] + 1 + (((line[1] - line[0]) % 3 + 1) % 3) * 3 for line in lines)
+score2 = sum(line[1] * 3 + (line[0] + line[1] - 1) % 3 + 1 for line in lines)
+
+print(score1, score2)
