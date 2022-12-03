f681b3220292bb09e0a87a0df149b5e6bd839b4c
Bruno Sacaric <bruno.sacaric@gmail.com>
2022-12-03 07:20:54 +0100

    day3 polish

--- 2022/tasks/3.py
+++ 2022/tasks/3.py
@@ -1,31 +1,27 @@
 with open("../input/3.txt") as f:
+    # lines = f.read().strip().split("\n")
     lines = f.readlines()
 
-lower = "abcdefghijklmnopqrstuvwxyz"
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-priorities = 0
+priorities1 = 0
 for line in lines:
     firstpart, secondpart = line[:len(line)//2], line[len(line)//2:]
     for c in firstpart:
         if c in secondpart:
             if ord(c) <= 90:
-                priorities += upper.index(c) + 27
+                priorities1 += ord(c) - 38
             else:
-                priorities += lower.index(c) + 1
+                priorities1 += ord(c) - 96
             break
 
-print(priorities)
-
-priorities = 0
+priorities2 = 0
 for i in range(0, len(lines), 3):
     line = lines[i]
     for c in line:
         if c in lines[i+1] and c in lines[i+2]:
             if ord(c) <= 90:
-                priorities += upper.index(c) + 27
+                priorities2 += ord(c) - 38
             else:
-                priorities += lower.index(c) + 1
+                priorities2 += ord(c) - 96
             break
 
-print(priorities)
+print(priorities1, priorities2)
